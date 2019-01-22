caa9067eea89901717bf087330721ae4664c1ef5
TomGao2001 <gzm2001@outlook.com>
2019-01-21 23:00:43 -0500

    working env, almost done wh

--- Practice/Chapter 1/wormhole.java
+++ Practice/Chapter 1/wormhole.java
@@ -6,6 +6,7 @@ import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Scanner;
 
 /*
@@ -13,102 +14,83 @@ LANG: JAVA
 PROB: wormhole
 */
 public class wormhole {
-    private static int N;
-    /*public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
+    private static int N, stuckPairs = 0;
+    private static long[][] holes;
+    public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
         Scanner sc = new Scanner(new File("wormhole.in"));
         PrintWriter pw = new PrintWriter("wormhole.out", "UTF-8");
 
         N = Integer.parseInt(sc.nextLine());
-        int[][] holes = new int[N][2];//each 2 are paired
-
+        holes = new long[N][2];
         for(int i = 0;i < N; i++){
-            String[] s = sc.nextLine().split(" ");
-            holes[i] = new int[] {Integer.parseInt(s[0]), Integer.parseInt(s[1])};
+            String[] tmp = sc.nextLine().split(" ");
+            holes[i][0] = Integer.parseInt(tmp[0]);
+            holes[i][1] = Integer.parseInt(tmp[1]);
         }
-        sc.close();//imported all WH locations
-        int result = 0;
-
-        ArrayList<int[][]> allFarms = new ArrayList<>(10395);
-
-
-        for(int[][] farm : allFarms){
-            //one layout
-            for(int i = 0;i < N; i++){
-                //one case (1 cow position, 1 layout)
-                int[] bessie = new int[2];
-                int[] temp = farm[1];
-                bessie[0] = temp[0];
-                bessie[1] = temp[1];
-                int pos;
-                for(int j = 0;j < 100;j++){
-                    pos = WHToRight(farm, bessie[0], bessie[1]);
-                    if(pos == -1)
-                        break;
-                    pos = findExit(pos);
-                    if(j == 99)
-                        result++;
-                }
-            }
-        }
-
-
+        Arrays.sort(holes, (o1, o2) -> (int)(o1[0] - o2[0]));
+        genAllPairs(null, 0, null);
+        pw.print(stuckPairs);
+        pw.close();
 
     }
+    private static void genAllPairs(int[] currentCfg, int n, int[] used){
+        if(n == 0) {
+            currentCfg = new int[N];
+            currentCfg[0] = 0;
+            for(int i = 1; i < N; i++)
+                currentCfg[i] = -1;
+            used = new int[N/2];
+            used[0] = 1;
+            for(int i = 1; i < N/2; i++)
+                used[i] = 0;
 
-    private static int WHToRight(int[][] holes, int locX, int locY){
-        int[] dists = new int[N];
-
-        for(int i = 0;i < N; i++){
-            int[] wh = holes[i];
-            if(wh[1] == locY && wh[0] >= locX){
-                dists[i] = Math.abs(wh[0] - locX);
+            genAllPairs(currentCfg, 1, used);
+        }else if(n == N-1){
+            for(int i = 0; i < N/2; i++){
+                if(used[i] == 1){
+                    currentCfg[N-1] = used[i];
+                    detectStuck(currentCfg);
+                    return;
+                }
             }
-        }
-        int min = Integer.MAX_VALUE;
-        int minIndex = -1;
-        for(int i = 0;i < N; i++){
-            if(dists[i] != 0){
-                if(min > dists[i]){
-                    min = dists[i];
-                    minIndex = i;
+        }else{
+            for(int i = 0;i < N/2; i++){
+                if(used[i] != 2){
+                    used[i]++;
+                    currentCfg[n] = i;
+                    genAllPairs(currentCfg, n+1, used);
                 }
             }
         }
-        if(minIndex != -1)
-            return minIndex;
+    }
+    private static void detectStuck(int[] config){//counts 1 config of pairs
+        final int iter = 1000;
+        for(int i = 0;i < N; i++){//i is current hole idx
+            int holeIter = 0;
+            int currentHoleIdx = 0;
+            while(holeIter < iter){
+                currentHoleIdx = findOutWormHole(config, currentHoleIdx);
+                if(currentHoleIdx == -1)
+                    return;
+                holeIter++;
+            }
+        }
+        stuckPairs++;
+    }
+
+    private static int findOutWormHole(int[] config, int currentHole){//finds the corresponding hole of WH to the right; if none returns -1
+        long[] currentLocation = holes[currentHole];
+        for(int i = 0;i < N; i++){
+            long[] tmp = holes[i];
+            if(tmp[0] > currentLocation[0] && tmp[1] == currentLocation[1]){
+                for(int j = 0;j < N; j++){
+                    if(j != i && config[j] == config[i]){
+                        return j;
+                    }
+                }
+            }
+        }
         return -1;
     }
 
-    private static int findExit(int index){
-        if(index % 2 == 0)
-            return index - 1;
-        else
-            return index + 1;
-    }
-
-    private static ArrayList<int[][]> getPermutations(int[][] holes){
-        if(N == 2){
-            ArrayList<int[][]> a = new ArrayList<>(1);
-            a.set(0, holes);
-            return a;
-        }
-        int possibilities = (factorial(N))/(factorial(N/2) * (int)Math.pow(2, N/2));
-        int[][][] result = new int[possibilities][N][2];
-        for(int i = 0;i < N - 2;i++){
-
-        }
-    }
-
-    private static boolean checkNotRepSolution(int[][] solution, ArrayList<int[][]> set){
-
-    }
-
-    private static int factorial(int target){
-        int product = 1;
-        for(int i = 2;i <= target; i++){
-            product *= i;
-        }
-        return product;
-    }
-*/
 }
