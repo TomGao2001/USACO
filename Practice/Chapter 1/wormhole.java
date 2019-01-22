043d507058d8350cd0926f88ab65264fa0868a6d
TomGao2001 <gzm2001@outlook.com>
2019-01-22 09:42:34 -0500

    wormhole working solution

--- Practice/Chapter 1/wormhole.java
+++ Practice/Chapter 1/wormhole.java
@@ -1,12 +1,10 @@
-import org.jetbrains.annotations.Nullable;
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.Scanner;
 
 /*
@@ -16,6 +14,7 @@ PROB: wormhole
 public class wormhole {
     private static int N, stuckPairs = 0;
     private static long[][] holes;
+    private static ArrayList<int[]> configs = new ArrayList<>(100000);
     public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
         Scanner sc = new Scanner(new File("wormhole.in"));
         PrintWriter pw = new PrintWriter("wormhole.out", "UTF-8");
@@ -28,54 +27,87 @@ public class wormhole {
             holes[i][1] = Integer.parseInt(tmp[1]);
         }
         Arrays.sort(holes, (o1, o2) -> (int)(o1[0] - o2[0]));
-        genAllPairs(null, 0, null);
-        pw.print(stuckPairs);
+        int[] currentCfg = new int[N];
+        for(int i = 0; i < N; i++)
+            currentCfg[i] = -1;
+        boolean[] used = new boolean[N/2];
+        for(int i = 0; i < N/2; i++)
+            used[i] = false;
+        genAllPairs(currentCfg, 0, used);
+        pw.println(stuckPairs);
+        /*for(int[] i : configs){
+            for(int j : i){
+                pw.print(j + " ");
+            }
+            pw.println();
+        }*/
         pw.close();
 
     }
-    private static void genAllPairs(int[] currentCfg, int n, int[] used){
-        if(n == 0) {
-            currentCfg = new int[N];
+    private static void genAllPairs(final int[] currentCfg, final int nPairs, final boolean[] used){
+        if(nPairs == 0){
             currentCfg[0] = 0;
-            for(int i = 1; i < N; i++)
-                currentCfg[i] = -1;
-            used = new int[N/2];
-            used[0] = 1;
-            for(int i = 1; i < N/2; i++)
-                used[i] = 0;
+            used[0] = true;
+            for(int i = 1; i < N; i++){
+                boolean[] tempUsed = used.clone();
+                int[] tmpCfg = currentCfg.clone();
+                tmpCfg[i] = 0;
+                genAllPairs(tmpCfg, 1, tempUsed);
+            }
+        }else if(nPairs == N/2-1){
+            detectStuck(currentCfg);
+        }else{
+            int nextUp = findNextUnused(used);
+            currentCfg[findNextNullLoc(currentCfg)] = nextUp;
+            used[nextUp] = true;
+            int[] nextIdxs = findNullLocs(currentCfg, nPairs);
+            for(int i : nextIdxs){
+                int[] tmp = currentCfg.clone();
+                tmp[i] = nextUp;
+                boolean[] tempUsed = used.clone();
+                genAllPairs(tmp, 1 + nPairs, tempUsed);
+            }
+        }
 
-            genAllPairs(currentCfg, 1, used);
-        }else if(n == N-1){
+        /*if(n == N - 1){
             for(int i = 0; i < N/2; i++){
                 if(used[i] == 1){
-                    currentCfg[N-1] = used[i];
-                    detectStuck(currentCfg);
+                    currentCfg[N-1] = i;
+                    if(cfgListHasNoEntry(currentCfg, configs)){
+                        configs.add(currentCfg.clone());
+                        detectStuck(currentCfg);
+                    }
                     return;
                 }
             }
         }else{
             for(int i = 0;i < N/2; i++){
                 if(used[i] != 2){
-                    used[i]++;
-                    currentCfg[n] = i;
-                    genAllPairs(currentCfg, n+1, used);
+                    int[] usedTmp = used.clone();
+                    usedTmp[i]++;
+                    int[] cfgTmp = currentCfg.clone();
+                    cfgTmp[n] = i;
+                    genAllPairs(cfgTmp, n+1, usedTmp);
                 }
             }
-        }
+        }*/
     }
     private static void detectStuck(int[] config){//counts 1 config of pairs
         final int iter = 1000;
         for(int i = 0;i < N; i++){//i is current hole idx
             int holeIter = 0;
-            int currentHoleIdx = 0;
+            int currentHoleIdx = findOutWormHole(config, i);
             while(holeIter < iter){
-                currentHoleIdx = findOutWormHole(config, currentHoleIdx);
                 if(currentHoleIdx == -1)
-                    return;
+                    break;
+                currentHoleIdx = findOutWormHole(config, currentHoleIdx);
                 holeIter++;
             }
+            if(currentHoleIdx != -1) {
+                stuckPairs++;
+                return;
+            }
         }
-        stuckPairs++;
     }
 
     private static int findOutWormHole(int[] config, int currentHole){//finds the corresponding hole of WH to the right; if none returns -1
@@ -93,4 +125,31 @@ public class wormhole {
         return -1;
     }
 
+    private static int findNextNullLoc(int[] config){
+        for(int i = 0;i < config.length; i++){
+            if(config[i] == -1)
+                return i;
+        }
+        return -1;
+    }
+
+    private static int findNextUnused(boolean[] used){
+        for(int i = 0;i < used.length; i++){
+            if(!used[i])
+                return i;
+        }
+        return -1;
+    }
+
+    private static int[] findNullLocs(int[] config, int nPairs){
+        int[] stuff = new int[N - 1 - 2 * nPairs];
+        int stuffIdx = 0;
+        for(int i = 0;i < config.length; i++)
+            if(config[i] == -1){
+                stuff[stuffIdx] = i;
+                stuffIdx++;
+            }
+
+        return stuff;
+    }
 }
